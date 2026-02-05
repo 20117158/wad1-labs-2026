e93476d2d07d46d407ee5ba2669c0ed0c966c8c6
Ben <bentay11@icloud.com>
2026-02-05 16:25:14 +0000

    First commit

---
+++ controllers/start.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import logger from "../utils/logger.js";
+
+
+const start = {
+  createView(request, response) {
+    response.send('Welcome to the Playlist app!');   
+  },
+};
+
+export default start;
