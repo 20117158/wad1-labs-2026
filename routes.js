144e488269fd7d7a8efc9a650dfe0dc5780239db
Ben <bentay11@icloud.com>
2026-03-12 14:53:11 +0000

    Completed Add Functions Lab

--- routes.js
+++ routes.js
@@ -14,5 +14,7 @@ router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
 router.get('/error', (request, response) => response.status(404).end('Page not found.'));
 router.get('/playlist/:id', playlist.createView);
+router.post('/playlist/:id/addsong', playlist.addSong);
+router.post('/dashboard/addplaylist', dashboard.addPlaylist);
 
 export default router;
