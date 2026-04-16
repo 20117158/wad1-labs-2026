8dd26d2a4b25e426e520ae03419671dbd1991879
Ben <bentay11@icloud.com>
2026-04-16 15:29:21 +0100

    User authentication added

--- controllers/about.js
+++ controllers/about.js
@@ -2,18 +2,24 @@
 
 import employees from "../models/employees.js";
 import logger from "../utils/logger.js";
+import accounts from './accounts.js';
 
 const about = {
-  createView(request, response) {
+createView(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
     logger.info("About page loading!");
     
-    const viewData = {
-      title: "Playlist App About",
-      employees: employees.getAppInfo()
-    };
-    
-    response.render('about', viewData);
-  },
+    if (loggedInUser) {
+      const viewData = {
+        title: 'About the Playlist App',
+        fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
+        employees: employees.getAppInfo(),
+      };
+      response.render('about', viewData);
+    }
+    else response.redirect('/');    
+},
+
 };
 
 export default about;
